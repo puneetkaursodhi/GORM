4b78979a8b0a54955489986869b8f5e1cbc9b3a8
Puneet <puneet@intelligrape.com>
2017-04-25 21:52:21 +0530

    Removed extra methods

--- grails-app/init/gorm/BootStrap.groovy
+++ grails-app/init/gorm/BootStrap.groovy
@@ -5,13 +5,6 @@ class BootStrap {
     def init = { servletContext ->
         List<User> users = createUsers()
         validate(users.first())
-        users.each {
-            findById(it.id)
-        }
-        users.each {
-            getById(it.id)
-        }
-        findByProperty()
     }
 
     List<User> createUsers() {
@@ -36,33 +29,6 @@ class BootStrap {
         log.info "---User email is valid :: ${user.validate(['email'])}"
         log.info "---After validate HasErrors---${user.hasErrors()}"
         user.save(validate: false)
-    }
-
-    void findById(Long id) {
-        log.info "#######Finding User with Id :${id}##########"
-        User user1 = User.findById(id)
-        log.info "_______Finding User with Id :${id}___________"
-        User user2 = User.findById(id)
-        log.info "---------------------------------------------"
-    }
-
-    void getById(Long id) {
-        log.info "#######Getting User with Id :${id}##########"
-        User user1 = User.get(id)
-        log.info "_______Getting User with Id :${id}___________"
-        User user2 = User.get(id)
-        log.info "---------------------------------------------"
-    }
-
-    void findByProperty() {
-        User user = User.findByName("user 1")
-        log.info "user---${user.id}---"
-        User user1 = User.findByNameIlikeAndBalance("%user%", 5000)
-        log.info "user1---${user1.id}---"
-        User user2 = User.findByNameLikeOrBalance("%user%", 5000)
-        log.info "user2---${user2.id}---"
-        User user3 = User.findByNameIlikeOrBalance("%user%", 5000, [sort: 'balance', order: 'desc'])
-        log.info "user3---${user3.id}---"
     }
 
 
